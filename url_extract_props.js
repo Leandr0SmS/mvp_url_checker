019dd426a2294111804527518dc3cf8e2850b74b
Leandro_SmS <leandrosimoesms@gmail.com>
2023-11-15 13:56:03 -0300

    Feat: Update data to remove column, added function to check url props

--- url_extract_props.js
+++ url_extract_props.js
@@ -1,13 +1,15 @@
 const dataJson = require('./dataset_phishing_json');
 
-const urlLenCount = url => url.href.length;
+const urlLenCount = url => url.length;
 
 const urlLenHostname = url => url.hostname.length;
 
-const urlNbCountChar = (url, char) => {
-    const urlString = url.href;
-    const regExp = new RegExp("\\" + `${char}`, "g");
-    const found = urlString.match(regExp);
+const urlNbCountChar = (url, char, scape) => {
+    let regExp;
+    scape
+        ? regExp = new RegExp("\\" + `${char}`, "g")
+        : regExp = new RegExp(`${char}`, "g")
+    const found = url.match(regExp);
     if (found) {
         return found.length
     } else {
@@ -19,65 +21,76 @@ const url_checker = (dataSet) => {
     const result = [];
     for (let data of dataSet) {
         const check = {};
-        const urlTest = new URL(data.url)
+        const urlTestObj = new URL(data.url)
+        const urlTest = data.url
 
-        data.url.length == data.length_url
+        check.url = urlTest
+
+        urlLenCount(urlTest) == data.length_url
             ? check.length_url = true
             : check.length_url = false
         
-        urlLenHostname(urlTest) == data.length_hostname
+        urlLenHostname(urlTestObj) == data.length_hostname
             ? check.length_hostname = true
             : check.length_hostname = false
 
-        urlNbCountChar(urlTest, "$") == data.nb_dollar
+        urlNbCountChar(urlTest, "$", true) == data.nb_dollar
             ? check.nb_dollar = true
             : check.nb_dollar = false
 
-        urlNbCountChar(urlTest, ".") == data.nb_dots
+        urlNbCountChar(urlTest, ".", true) == data.nb_dots
             ? check.nb_dots = true
             : check.nb_dots = false
 
-        urlNbCountChar(urlTest, "-") == data.nb_hyphens
+        urlNbCountChar(urlTest, "-", true) == data.nb_hyphens
             ? check.nb_hyphens = true
             : check.nb_hyphens = false
         
-        urlNbCountChar(urlTest, "_") == data.nb_underscore
+        urlNbCountChar(urlTest, "_", true) == data.nb_underscore
             ? check.nb_underscore = true
             : check.nb_underscore = false
 
-        urlNbCountChar(urlTest, "~") == data.nb_tilde
+        urlNbCountChar(urlTest, "~", true) == data.nb_tilde
             ? check.nb_tilde = true
             : check.nb_tilde = false
         
-        urlNbCountChar(urlTest, "%") == data.nb_percent
+        urlNbCountChar(urlTest, "%", true) == data.nb_percent
             ? check.nb_percent = true
             : check.nb_percent = false
         
-        urlNbCountChar(urlTest, "/") == data.nb_slash
+        urlNbCountChar(urlTest, "/", true) == data.nb_slash
             ? check.nb_slash = true
             : check.nb_slash = false
 
-        urlNbCountChar(urlTest, ":") == data.nb_colon
+        urlNbCountChar(urlTest, ":", true) == data.nb_colon
             ? check.nb_colon = true
             : check.nb_colon = false
         
-        urlNbCountChar(urlTest, ",") == data.nb_comma
+        urlNbCountChar(urlTest, ",", true) == data.nb_comma
             ? check.nb_comma = true
             : check.nb_comma = false
         
-        urlNbCountChar(urlTest, ";") == data.nb_semicolumn
+        urlNbCountChar(urlTest, ";", true) == data.nb_semicolumn
             ? check.nb_semicolumn = true
             : check.nb_semicolumn = false
 
-        urlNbCountChar(urlTest, "\s") == data.nb_space
-            ? check.nb_space = true
-            : check.nb_space = false
+        urlNbCountChar(urlTest, "www", false) == data.nb_www
+            ? check.nb_www = true
+            : check.nb_www = false
+
+        const urlOriginLen = urlTestObj.origin.length;
+        const path = urlTest.substring(urlOriginLen);
+        urlNbCountChar(path, "http", false) == data.http_in_path
+            ? check.http_in_path = true
+            : check.http_in_path = false
 
         result.push(check)
     }
     return result.map(d => {
         if (Object.values(d).some(value => value === false)) {
             return d
+        } else {
+            return true
         }
     })
 }
