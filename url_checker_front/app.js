ebe434cee13a93c39909d23dcd29bd6082e0392b
Leandro_SmS <leandrosimoesms@gmail.com>
2023-11-21 21:54:15 -0300

    styles: Set styles to input, title and btn

--- url_checker_front/app.js
+++ url_checker_front/app.js
@@ -1,20 +1,55 @@
 const { useState, useEffect } = React;
 const { createRoot } = ReactDOM;
 
-const UrlCheck = () => {
+const UrlCheck = ({onClickHandle, inputChangeHandler, inputValue}) => {
     return (
         <form id="url_form">
-            <input className="url_input" id="url_Input" type="input"/>
-            <button type="button" id="url_check_btn">Check</button>
+            <input 
+                className="url_input" 
+                id="url_Input" 
+                type="input"
+                onChange={inputChangeHandler}
+                value={inputValue}
+            />
+            <button 
+                type="button" 
+                id="url_check_btn"
+                onClick={onClickHandle}
+            >
+                Check
+            </button>
         </form>
     )
 }
 
 const App = () => {
+
+    const [urlToCheck, setUrlToCheck] = useState("");
+    const [urlStatus, setUrlStatus] = useState("");
+
+    const handleInputChange = (e) => {
+        setUrlToCheck(e.target.value);
+    }
+
+    const handleCheckClick = () => {
+        if (urlToCheck) {
+            const result = checkerURL(urlToCheck);
+            setUrlStatus(result);
+            console.log(`URL Checked: ${result.url}`)
+        } else {
+            console.log(`URL Checked: Fail`)
+        }
+    }
+
     return (
         <React.Fragment>
             <h1 id="title">URL CHECK</h1>
-            <UrlCheck/>
+            <UrlCheck
+                inputChangeHandler={handleInputChange}
+                inputValue={urlToCheck}
+                onClickHandle={handleCheckClick}
+            />
+            <h2>{urlStatus}</h2>
         </React.Fragment>
     )
 }
